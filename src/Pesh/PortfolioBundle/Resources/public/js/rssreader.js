0f77750cc2201d75c070b56b2ed39b867b978c33
Peshmelba <contact.peshmelba@gmail.com>
2015-04-29 00:41:38 +0200

    Rss client working on index page. Index page done.

--- src/Pesh/PortfolioBundle/Resources/public/js/rssreader.js
+++ src/Pesh/PortfolioBundle/Resources/public/js/rssreader.js
@@ -35,7 +35,8 @@ function mergeLocalFeeds(tags)
 
 function loadFeeds()
 {
-	for (var i=0; i < localFeeds.length; i++)
+	var count = 0;
+	for (var i = 0; i < localFeeds.length; i++)
 	{
 		$.ajax({
 			url : ajaxproxy + encodeURIComponent(encodeURIComponent(localFeeds[i].src)),
@@ -43,7 +44,70 @@ function loadFeeds()
 			dataType : "xml",
 			success : function(feed) {
 				items.push.apply(items, $(feed).find("item"));
+
+				count++;
+
+				if (count == localFeeds.length){
+					sortByDate(false);
+
+					for (var j = 0; j < 180; j++){
+						printItem(items[j]);
+					}
+				}
 			}
 		});
 	}
+}
+
+function sortByDate(distinct)
+{
+	items.sort(compare);
+
+	distinct = typeof distinct !== 'undefined' ? distinct : false;
+	
+	if(distinct)
+		for (var j,i = 0; i < (items.length-1); i++)
+			for (j = items.length; j > i+1; j--)
+				if ($(items[i]).find('link') == $(items[j]).find('link'))
+					items.splice(j, 1);
+
+}
+
+function compare(a,b)
+{
+	if (getDate(a) < getDate(b))
+		return 1;
+	if (getDate(a) > getDate(b))
+		return -1;
+	return 0;
+}
+
+function getDate(item)
+{
+	return new Date($(item).find('pubDate').text());
+}
+
+function printItem(item)
+{
+	divItem = document.createElement('div');
+	divItem.className = "item";
+
+	pItemTitle = document.createElement('p');
+	pItemTitle.className = "item-title";
+
+	aItemLink = document.createElement('a');
+	aItemLink.className = "item-link";
+	aItemLink.href = $(item).find('link').text();
+	aItemLink.innerHTML = $(item).find('title').text();
+
+	pItemDate = document.createElement('p');
+	pItemDate.className = "item-date";
+	pItemDate.innerHTML = (new Date($(item).find('pubDate').text())).toLocaleString();
+
+	pItemDescription = document.createElement('p');
+	pItemDescription.className = "item-description";
+	pItemDescription.innerHTML = $(item).find('description').text();
+
+	$(divItem).append($(pItemTitle).append(aItemLink), pItemDate, pItemDescription, document.createElement('hr'));
+	$('#rss-viewer').append(divItem);
 }
